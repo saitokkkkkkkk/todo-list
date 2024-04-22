4d7c1d352f7a4c0faa311bf2b0bf3ac0f67fa215
saitokkkkkkkk <k_saito@craid-inc.com>
2024-04-22 14:43:39 +0900

    追加の処理を関数に。空文字対策とエスケープ。

--- code/add_todo.php
+++ code/add_todo.php
@@ -5,30 +5,49 @@
 -->
 
 <?php
-// DB接続
-require_once 'db_connection.php';
 
-// テーブルに行を追加する関数
+// データを追加する関数
 function addTodoToDatabase($dbname, $title, $content) {
-    // 上の関数を動かしてDB接続し、戻り値(オブジェクト？)を取得
+    // DB接続
+    require_once 'db_connection.php';
     $conn = connectToDatabase($dbname);
-    // sqlの準備
-    $sql = "INSERT INTO todos (title, content) VALUES ('$title', '$content')";
-    // sqlの実行とその後
-    if ($conn->query($sql) === TRUE) {
-        echo "新しい行が正常に挿入されました。";
-    } else {
+
+    // sqlの準備(これもstmtで)
+    $sql = "INSERT INTO todos (title, content) VALUES (?, ?)";
+    // stmtの準備
+    $stmt = $conn->prepare($sql);
+    // パラメータのバインド
+    $stmt->bind_param("ss", $title, $content);
+
+    // stmtの実行とその後
+    if ($stmt->execute()){ 
+        // DBとの接続とstmtを閉じておく
+        $conn->close();
+        $stmt->close();
+        // リダイレクト(getメソッドでパラメータaddを渡す)
+        header("Location: todo_list.php?message=add");
+    }else{
         echo "エラー: " . $sql . "<br>" . $conn->error;
     }
-    // DBとの接続を閉じる
-    $conn->close();
+
 }
 
-// フォームを受け取る
-$title = $_POST['title'];
-$content = $_POST['content'];
-
-// 受け取ったフォームを元に、DBに行を追加
-addTodoToDatabase($dbname, $title, $content);
-
-
+// パラメータの受け取り、そのエスケープ、上の関数の呼び出し
+// リクエストメソッドの確認(postか否か)
+if($_SERVER["REQUEST_METHOD"]=== "POST"){
+    // 必要なデータを受け取れているか、かつ空文字ではないか
+    if(isset($_POST['title'], $_POST['content']) && $_POST['title'] !== '' && $_POST['content'] !== ''){
+        // そのデータをエスケープして変数に格納(sqlインジェクション対策)
+        $title = mysqli_real_escape_string($conn, $_POST['title']);
+        $content = mysqli_real_escape_string($conn, $_POST['content']);
+        // もらったデータを引数にして関数を呼ぶ
+        addTodoToDatabase($dbname, $title, $content);
+    }else{
+        // うまくいってない原因を画面に表示
+        echo "フォームが不完全です";
+    }
+}else{
+    // 関数を呼ばずにリダイレクト（原因はログに表示。ちゃんと動く？）
+    error_log("POST リクエストではありません。");
+    header("Location: add_todo.php");
+}
