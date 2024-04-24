c1bf6ccd354af48f77e3e2b0300bf88124ed95f6
saitokkkkkkkk <k_saito@craid-inc.com>
2024-04-24 13:50:03 +0900

    redirect_config.phpの追加とファイル名の変更

---
+++ config/redirect_config.php
@@ -0,0 +1,6 @@
+<?php
+define("REDIRECT_DELETE", "todo_list.php?message=deleted");
+define("REDIRECT_UPDATE", "todo_list.php?message=update");
+define("REDIRECT_UPDATE_FAILED", "todo_list.php?message=update_failed");
+define("REDIRECT_ADD", "todo_list.php?message=add");
+define("REDIRECT_ADD_FAILED", "todo_list.php?message=add_failed");
